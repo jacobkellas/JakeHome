7c6203fe173f69085fcbfcedebb1fdd0a8aa1952
Jake Kellas <83732510+jacobkellas@users.noreply.github.com>
2022-04-23 09:49:52 -0700

    Test authentication

--- ui/src/components/Toolbar.vue
+++ ui/src/components/Toolbar.vue
@@ -1,24 +1,22 @@
 <template>
-  <q-toolbar>
-    <q-btn
-      flat
-      stretch
-      size="lg"
-      class="text-weight-medium"
-      @click="$router.push('/')"
-    >
-      Vue Playground
-    </q-btn>
-    <q-btn flat stretch label="V-Model Demo" @click="$router.push('/vmodel')" />
-    <a href="/.auth/login/aad">Login</a>
-    <q-space />
-    <q-btn
-      flat
-      stretch
-      :label="$q.dark.isActive ? 'light' : 'dark'"
-      @click="toggleDark"
-    />
-  </q-toolbar>
+  <q-btn
+    flat
+    stretch
+    size="lg"
+    class="text-weight-medium"
+    @click="$router.push('/')"
+  >
+    Vue Playground
+  </q-btn>
+  <q-btn flat stretch label="V-Model Demo" @click="$router.push('/vmodel')" />
+  <q-btn flat stretch label="Login" @click="handleLogin" />
+  <q-btn flat stretch label="Logout" @click="handleLogout" />
+  <q-btn
+    flat
+    stretch
+    :label="$q.dark.isActive ? 'light' : 'dark'"
+    @click="toggleDark"
+  />
 </template>
 
 <script setup lang="ts">
@@ -26,7 +24,13 @@ import { useQuasar } from 'quasar'
 
 const $q = useQuasar()
 
-function toggleDark() {
+function toggleDark(): void {
   $q.dark.toggle()
+}
+function handleLogin(): void {
+  window.location.href = '/.auth/login/aad'
+}
+function handleLogout(): void {
+  window.location.href = '/.auth/logout'
 }
 </script>
--- ui/src/views/Home.vue
+++ ui/src/views/Home.vue
@@ -9,7 +9,7 @@
         <q-card-section>
           <div class="text-body1 text-center">Click on the demos above.</div>
           <div v-if="secret" class="text-body1 text-center">{{ secret }}</div>
-          <q-btn @click="handleEnv">Test</q-btn>
+          <q-btn @click="getUserInfo">Test</q-btn>
           <q-btn @click="handleFunction">Test Function</q-btn>
         </q-card-section>
       </q-card>
@@ -20,12 +20,12 @@
 <script setup lang="ts">
 const secret: string = process.env.VUE_APP_TEST_SETTING
 
-function handleEnv(): void {
-  console.log(process.env)
-}
 async function handleFunction(): Promise<void> {
   await fetch('/api/HttpTriggerWithOpenAPICSharp1?name=Jake').then((response) =>
     console.log(response)
   )
+}
+function getUserInfo(): void {
+  fetch('/.auth/me').then((response) => console.log(response.json()))
 }
 </script>
