7312cb7b42a874b4a6e89587053a07df78ef0293
Jake Kellas <83732510+jacobkellas@users.noreply.github.com>
2022-04-23 18:16:21 -0700

    Create typed store

--- ui/src/views/Home.vue
+++ ui/src/views/Home.vue
@@ -8,9 +8,8 @@
         <q-separator inset />
         <q-card-section>
           <div class="text-body1 text-center">Click on the demos above.</div>
-          <div v-if="secret" class="text-body1 text-center">{{ secret }}</div>
-          <q-btn @click="getUserInfo">Test</q-btn>
-          <q-btn @click="handleFunction">Test Function</q-btn>
+          <q-btn color="primary" @click="getUserInfo">Test</q-btn>
+          <q-btn color="primary" @click="handleFunction">Test Function</q-btn>
         </q-card-section>
       </q-card>
     </div>
@@ -18,8 +17,6 @@
 </template>
 
 <script setup lang="ts">
-const secret: string = process.env.VUE_APP_TEST_SETTING
-
 async function handleFunction(): Promise<void> {
   await fetch('/api/HttpTriggerWithOpenAPICSharp1?name=Jake').then((response) =>
     console.log(response)
