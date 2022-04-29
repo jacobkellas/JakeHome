c21707e194723ca902fd330753894c4a89630ed8
Jake Kellas <83732510+jacobkellas@users.noreply.github.com>
2022-04-28 19:48:38 -0700

    feat: save dark mode to localStorage

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -11,15 +11,21 @@
 
 <script setup lang="ts">
 import Toolbar from '@/components/Toolbar.vue'
+import { useQuasar } from 'quasar'
 import { onBeforeMount, computed, ref } from 'vue'
 import { authentication } from '@/composables/authentication'
 import { useStore } from '@/store'
 
+const $q = useQuasar()
 const { getUser, isAuthenticated } = authentication()
 const store = useStore()
 const isUserAuthenticated = ref(computed(() => isAuthenticated()))
 
 onBeforeMount(async () => {
+  const darkMode = store.getters.getDarkMode
+  if (darkMode && !$q.dark.isActive) {
+    $q.dark.toggle()
+  }
   const user = await getUser()
   if (user) {
     store.dispatch('updateUserInfo', user)
